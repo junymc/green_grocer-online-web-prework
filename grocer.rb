fa9d04e51f07f54bc6628eaf5a6a3f99b10a2454
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-06 20:33:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,30 +13,50 @@ def consolidate_cart(cart)
   new_hash
 end
 
+# 
+# def apply_coupons(cart, coupons)
+#   #cart = {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}}
+#   #coupons = [{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
+#     coupons.each do |coupon_hash|
+#       item = coupon_hash[:item]
+#       new_coupon_hash = {
+#         :price => coupon_hash[:cost],
+#         :clearance => "true",
+#         :count => coupon_hash[:num]
+#       }
+#
+#       if cart.key?(item)
+#         new_coupon_hash[:clearance] = cart[item][:clearance]
+#         if cart[item][:count] >= new_coupon_hash[:count]
+#           new_coupon_hash[:count] = (cart[item][:count]/new_coupon_hash[:count]).floor
+#           cart[item][:count] = (coupon_hash[:num])%(cart[item][:count])
+#         end
+#         cart[item + " W/COUPON"] = new_coupon_hash
+#       end
+#     end
+#     return cart
+#   end
 
-def apply_coupons(cart, coupons)
-  #cart = {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}}
-  #coupons = [{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
+  def apply_coupons(cart, coupons)
     coupons.each do |coupon_hash|
-      item = coupon_hash[:item]
+      fruit_name = coupon_hash[:item]
       new_coupon_hash = {
         :price => coupon_hash[:cost],
         :clearance => "true",
         :count => coupon_hash[:num]
       }
 
-      if cart.key?(item)
-        new_coupon_hash[:clearance] = cart[item][:clearance]
-        if cart[item][:count] >= new_coupon_hash[:count]
-          new_coupon_hash[:count] = (cart[item][:count]/new_coupon_hash[:count]).floor
-          cart[item][:count] = (coupon_hash[:num])%(cart[item][:count])
+       if cart.key?(fruit_name)
+        new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
+        if cart[fruit_name][:count]>= new_coupon_hash[:count]
+          new_coupon_hash[:count] = (cart[fruit_name][:count]/new_coupon_hash[:count]).floor
+          cart[fruit_name][:count] = (coupon_hash[:num])%(cart[fruit_name][:count])
         end
-        cart[item + " W/COUPON"] = new_coupon_hash
+        cart[fruit_name + " W/COUPON"] = new_coupon_hash
       end
-    end
+      end
     return cart
   end
-
 
 
 def apply_clearance(cart)
