958a29d1f08397e2a6289a07d96847fc376b94f7
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-06 20:43:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,19 +55,6 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = consolidate_cart(cart: cart)
-     cart = apply_coupons(cart: cart, coupons: coupons)
-    cart = apply_clearance(cart: cart)
-    total = 0
-    cart.each do |item_name, item_data|
-      total += (item_data[:price] * item_data[:count])
-    end
-
-     if total > 100
-      0.9 * total
-    else
-      total
-    end
 
 
 end
