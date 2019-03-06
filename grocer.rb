cd8f61c1d696cb195b0639402a9e2b546f086410
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-06 20:36:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,28 +14,53 @@ def consolidate_cart(cart)
 end
 
 
+# def apply_coupons(cart, coupons)
+#   #cart = {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}}
+#   #coupons = [{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
+#     coupons.each do |coupon_hash|
+#       item = coupon_hash[:item]
+#       new_coupon_hash = {
+#         :price => coupon_hash[:cost],
+#         :clearance => "true",
+#         :count => coupon_hash[:num]
+#       }
+# 
+#       if cart.key?(item)
+#         new_coupon_hash[:clearance] = cart[item][:clearance]
+#         if cart[item][:count] >= new_coupon_hash[:count]
+#           new_coupon_hash[:count] = (cart[item][:count]/new_coupon_hash[:count]).floor
+#           cart[item][:count] = (coupon_hash[:num])%(cart[item][:count])
+#         end
+#         cart[item + " W/COUPON"] = new_coupon_hash
+#       end
+#     end
+#     return cart
+#   end
 def apply_coupons(cart, coupons)
-  #cart = {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}}
-  #coupons = [{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
-    coupons.each do |coupon_hash|
-      item = coupon_hash[:item]
-      new_coupon_hash = {
-        :price => coupon_hash[:cost],
-        :clearance => "true",
-        :count => coupon_hash[:num]
-      }
+  # code here
+  return cart if coupons == []
 
-      if cart.key?(item)
-        new_coupon_hash[:clearance] = cart[item][:clearance]
-        if cart[item][:count] >= new_coupon_hash[:count]
-          new_coupon_hash[:count] = (cart[item][:count]/new_coupon_hash[:count]).floor
-          cart[item][:count] = (coupon_hash[:num])%(cart[item][:count])
-        end
-        cart[item + " W/COUPON"] = new_coupon_hash
+  new_cart = cart
+  
+  coupons.each do |coupon|
+    name = coupon[:item]
+    num_of_c = coupon[:num]
+    if cart.include?(name) && cart[name][:count] >= num_of_c
+      new_cart[name][:count] -= num_of_c
+      if new_cart["#{name} W/COUPON"] == nil
+        new_cart["#{name} W/COUPON"] = {
+          price: coupon[:cost],
+          clearance: new_cart[name][:clearance],
+          count: 1
+          }
+      else
+        new_cart["#{name} W/COUPON"][:count] += 1
       end
     end
-    return cart
   end
+  #new_cart.delete_if { |name,data| data[:count] < 1 }
+  new_cart
+end
 
 
 
